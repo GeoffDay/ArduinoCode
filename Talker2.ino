b35c6462351cd795b354586dd3dbfd8cfda1b210
Geoff Day <gda26470@gmail.com>
2025-11-30 20:59:35 +1030

    Update Talker2.ino
    The other code was crap. This follows my standard one character and one or more hex digits and works well.

--- Talker2.ino
+++ Talker2.ino
@@ -1,77 +1,134 @@
-// This is Talker2 that takes serial text and transmits it over I2C to the other Uno board. 
-#include <LiquidCrystal.h>
+// This is Talker2 that takes sends a preset text and transmits it over I2C to the other Uno board. 
 #include <Wire.h>
 
-// initialize the library by associating any needed LCD interface pin with the arduino pin number it is connected to
-const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+byte SLAVE_ADDRESS = 0x09;      // address of the device I want to talk to
+byte REG_ADDRESS = 0x06;        // the register that sets the Capacitor shunt voltage
+byte READ_LENGTH = 0x02;        // 16 bit return
+unsigned int NEW_VALUE = 0x3FFF;         // what we want it to be idealy
 
-int ledPin = 13;              // select the pin for the LED
-char _bufferS[8] = "Hello!";             // for the serial text
-char _bufferW[8] = "Hello!";             // for the serial text
-int byteNum = 0;
-
-int deviceAddr = 0x09;      // address of the device I want to talk to
-int registerAddr = 0x06;       // the register I want to look at
-int dataI2C = 0;            // variable for the data to send or receive
-String intStr = String(0);          // string so that I can send ints
 
 void setup() {
-  lcd.begin(16, 2);          // set up the LCD's number of columns and rows:
-  lcd.display();             // idealy an i2c display would save a few pins.
+  Serial.begin(9600);      		  // initialize a serial port to talk: 
+  DisplayValues(); 
+  Wire.begin();                 // join I2C bus (address optional for master)
+}
 
-  Serial.begin(9600);      		// initialize a serial portt to talk:
+void DisplayValues() {
+  Serial.println("LTC3350 I2C programmer");
+  Serial.print("Slave Address is ");
+  Serial.println(SLAVE_ADDRESS, HEX); 
+  Serial.print("Reg Address is ");
+  Serial.println(REG_ADDRESS, HEX);
+  Serial.print("New Value is ");
+  Serial.println(NEW_VALUE, HEX);
   
-  pinMode(ledPin, OUTPUT);    	// declare the ledPin as an OUTPUT:
-  digitalWrite(ledPin, LOW); 	// turn the ledPin off to show error
-  
-  Wire.begin(); // join I2C bus (address optional for master)
+  Serial.print("Current Value ");
+  ReadValue();
+
+  Serial.println("Press Shh to change Slave Address, Rhh to change Register");
+  Serial.println("or Vhhhh to change Value where h is a hex digit.");
+  Serial.println("Press X to examine current value, P to program the new value");
+  Serial.println("or Space to display all this."); 
+  Serial.println();
 }
 
 
+void ReadValue() {
+  Wire.beginTransmission(SLAVE_ADDRESS);  
+  Wire.write(REG_ADDRESS);            // set register to read
+  Wire.endTransmission(false);        // false to not release the line
+
+  Wire.requestFrom(SLAVE_ADDRESS, READ_LENGTH); // request 2 bytes from register 
+  byte buff[READ_LENGTH];             // create a buffer 
+  Wire.readBytes(buff, READ_LENGTH);  // read into it
+  Wire.endTransmission(true);         // end this tx  
+
+  for (int i = 0; i < READ_LENGTH; i++) {
+    Serial.print(buff[i], HEX);    // print the initial 
+  }
+
+  Serial.println();
+}
+
+void ProgramValue() {
+  Wire.beginTransmission(SLAVE_ADDRESS);  // start another tx
+  Wire.write(REG_ADDRESS);                // set the register to change
+  Wire.write(NEW_VALUE / 256);
+  // Serial.print(NEW_VALUE / 256);
+  Wire.write(NEW_VALUE % 256);
+  // Serial.print(NEW_VALUE % 256);
+  Wire.endTransmission(true);              // end this one too.
+  
+  delay(100);
+
+  Serial.print("New Value ");
+  ReadValue();
+}
+
+byte GetHexByte(){
+  byte val = 0;
+
+  while (Serial.available()) {       // something arrived via serial connection?
+    delay(100);
+    byte c= Serial.read();        // read the new character
+
+    if (isHexadecimalDigit(c)) {
+      // hex character to the 4bit equivalent number, using the ascii table indexes
+      if (c >= '0' && c <= '9') c = c - '0';
+      else if (c >= 'a' && c <= 'f') c = c - 'a' + 10;
+      else if (c >= 'A' && c <= 'F') c = c - 'A' + 10;
+     
+      val = (val << 4) | (c & 0xF); // shift 4 to make space for new digit, and add the 4 bits of the new digit 
+    }
+  }  
+  return val;
+}
+
+
+unsigned int GetHexInt(){
+  unsigned int val = 0;
+
+  while (Serial.available()) {       // something arrived via serial connection?
+    delay(100);
+    byte c= Serial.read();        // read the new character
+
+    if (isHexadecimalDigit(c)) {
+      // hex character to the 4bit equivalent number, using the ascii table indexes
+      if (c >= '0' && c <= '9') c = c - '0';
+      else if (c >= 'a' && c <= 'f') c = c - 'a' + 10;
+      else if (c >= 'A' && c <= 'F') c = c - 'A' + 10;
+      
+      val = (val << 4) | (c & 0xF); // shift 4 to make space for new digit, and add the 4 bits of the new digit 
+    }
+  }  
+  return val;
+}
 
 void loop() {
-  if (Serial.available()) {
-    lcd.clear();                        // clear the screen
-    lcd.setCursor(0,0);
-
-    // read from port 0, send to I2C:
-    while (Serial.available()) {
-      delay(100);  
-      int inByte = Serial.read();       // read a byte
-      _bufferS[byteNum++] = inByte;     // stick the raw data into a buffer for the serial
-         
-      if (inByte != 0x0a) {             // strip off the line feed for lcd
-        lcd.write(inByte); 
-      }
-      // lcd.scrollDisplayRight();
-      if ((inByte > 64) & (inByte < 72)) {  // convert to the integer equiv.
-        inByte = inByte - 55 ;
-      } else if ((inByte > 96) & (inByte < 103)) {
-        inByte = inByte - 87;
-      } else if (isDigit(inByte)) {
-        inByte = inByte - 48;
-      }  
-
-      Wire.beginTransmission(0x08); 	// transmit to device #8
-      Wire.write(inByte);        	// sends a byte
-      Wire.endTransmission();    	// stop transmitting
-    }
- 
-  } 
-
-  if (Wire.available()) {
-    while(Wire.available()) {
-      int inByte = Wire.read();
-      _bufferW[byteNum++] = inByte;
+   if (Serial.available()) {}      // something arrived via serial connection?
       delay(100);
-
-    }
+      char c= Serial.read(); // read the new character
       
- 
-    Serial.println(_bufferW);
+      switch(c) {
+        case 'S': SLAVE_ADDRESS = GetHexByte();    // set new Slave address
+                  DisplayValues();   
+          break;
+        case 'R': REG_ADDRESS = GetHexByte();      // set new Register
+                  DisplayValues();
+          break;
+        case 'V': NEW_VALUE = GetHexInt();       // set new Value
+                  // Serial.println(NEW_VALUE);
+                  // Serial.println(NEW_VALUE, HEX);
+                  DisplayValues();
+          break;
+        case 'X': Serial.print("Current Value ");
+                  ReadValue();                  // Examine current settings
+          break;
+        case 'P': ProgramValue();               // program new Value
+          break;
+        case ' ': DisplayValues();
+          break;
+         default: 
+          break;
+      }
   }
-  byteNum = 0;
-
-
-}
